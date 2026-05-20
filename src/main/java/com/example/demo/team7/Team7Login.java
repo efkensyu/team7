1c7744a18329cfbdac7b35883398eefb8e428870
akasakakaito <k_akasaka@bold.ne.jp>
2026-05-20 14:15:36 +0900

    login code

---
+++ src/main/java/com/example/demo/team7/Team7Login.java
@@ -0,0 +1,13 @@
+package com.example.demo.team7;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Team7Login {
+	@GetMapping("test")
+	public String index() {
+		return "hello world";
+	}
+
+}
---
+++ src/main/java/com/example/demo/team7entity/Team7Account.java
@@ -0,0 +1,16 @@
+package com.example.demo.team7entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "user_tbl")
+@Data
+public class Team7Account {
+	@Id
+	private String userCd;
+	private String userPw;
+}
