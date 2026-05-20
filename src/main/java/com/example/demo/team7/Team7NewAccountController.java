40097ce616ee5187269a1023475468f7822249e6
morimotoo <h_morimoto@bold.ne.jp>
2026-05-20 14:11:06 +0900

    controller totyu

--- src/main/java/com/example/demo/team7/Team7NewAccountController.java
+++ src/main/java/com/example/demo/team7/Team7NewAccountController.java
@@ -24,41 +24,48 @@ public class Team7NewAccountController {
 	 public String login() {
 		return "Team7/Team7Login";
 	}
+	
 	 
 	//カレンダー画面に移動Post ようはログインしましたよ状態
-	@PostMapping(value = "/Team7Calender",params = "login")
+	@PostMapping(value = "/Team7_fromLogin",params = "login")
 	 	public String calender() {
 		return "Team7/Team7Calender";
 	}
 	
+	
+	
+	
 	//新規作成の画面に行くの
-	@PostMapping(value = "/Team7Login",params="make")
+	@PostMapping(value = "/Team7_fromLogin",params="make")
 		public String make() {
 		return "Team7/Team7MakeNewAccount";
 	}
+	
 	//新規画面からログイン画面に戻る
-	@PostMapping(value = "/Team7MakeNewAccount",params="backTop")
+	@PostMapping(value = "/Team7_fromNew",params="backTop")
 		public String back() {
 		return "Team7/Team7Login";
 	}
 	
 	//新規作成の確認ボタンを押し確認画面へ
-	@PostMapping(value = "/Team7MakeNewAccount",params="confilm")
+	@PostMapping(value = "/Team7_fromNew",params="confilm")
 		public String confilm() {
 		return "Team7/Team7Confirm";
-		}
+	}
 	
 	//確認画面から確定するボタンを押す。TOPにいく
-	@PostMapping(value = "/Team7Confirm",params="confilm")
+	@PostMapping(value = "/Team7_fromConfirm",params="confilm")
 		public String jikkou() {
 		return "Team7/Team7Login";
 	}
 	
 	//確認画面から修正する
-	@PostMapping(value = "/Team7Confilm",params="back")
-	public String bavk1() {
-	return "Team7/Team7MakeNewAccount";
+	@PostMapping(value = "/Team7_fromConfilm",params="back")
+		public String bavk1() {
+		return "Team7/Team7MakeNewAccount";
 	}
+
+	
 	
 	
 	
