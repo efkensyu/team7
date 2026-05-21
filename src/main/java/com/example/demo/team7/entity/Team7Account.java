53e894c8852d82fd8664249ad1048150187a43ca
akasakakaito <k_akasaka@bold.ne.jp>
2026-05-21 16:43:24 +0900

    akasaka

--- src/main/java/com/example/demo/team7/entity/Team7Account.java
+++ src/main/java/com/example/demo/team7/entity/Team7Account.java
@@ -3,8 +3,6 @@ package com.example.demo.team7.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -13,9 +11,6 @@ import lombok.Data;
 @Data
 public class Team7Account {
 	@Id
-	@NotBlank(message = "未入力項目があります。")
 	private String userCd;
-	@NotBlank(message = "未入力項目があります。")
-	@Size(min = 8, max = 20, message = "パスワードは8～20桁で入力してください")
 	private String userPw;
 }
