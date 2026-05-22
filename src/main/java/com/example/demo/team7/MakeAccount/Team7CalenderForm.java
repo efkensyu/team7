9227f37cee609e467c21212be7a7bfb53f470b92
morimotoo <h_morimoto@bold.ne.jp>
2026-05-22 13:52:42 +0900

    day4 evening

---
+++ src/main/java/com/example/demo/team7/MakeAccount/Team7CalenderForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.team7.MakeAccount;
+
+import lombok.Data;
+
+@Data
+public class Team7CalenderForm {
+	private String data;
+	private String sche;
+	private String detail;
+	
+
+}
