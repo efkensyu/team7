6e299b9261cb911ed561b99115a396a725514a77
akasakakaito <k_akasaka@bold.ne.jp>
2026-05-27 13:53:56 +0900

    login

--- src/main/java/com/example/demo/team7/MakeAccount/Team7Form.java
+++ src/main/java/com/example/demo/team7/MakeAccount/Team7Form.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Data
 public class Team7Form {
 	@NotBlank(message = "IDを入力してください。")
-    @Pattern(regexp= "^(|.{4,8})$", message = "IDは4文字以上8文字以内です。")
+    @Pattern(regexp = "^[0-9]{4,8}$", message = "IDは4〜8桁の数字で入力してください")
 	private String userCd;
 	
 	@NotBlank(message = "パスワードを入力してください。")
